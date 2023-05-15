d09c19c62ef239b5f0cbad088ee80a04eb795fcf
Sicherheitsschmiede <ui.manufaktur@gmail.com>
2023-05-15 02:51:01 +0200

    Rename

--- source/apps/retail/views/package.d
+++ source/apps/retail/views/package.d
@@ -1,6 +1,6 @@
-module apps.myname.views;
+module apps.retail.views;
 
 public { // Packages
-  import apps.myname.views.actions;
-  import apps.myname.views.forms;
+  import apps.retail.views.actions;
+  import apps.retail.views.forms;
 }
